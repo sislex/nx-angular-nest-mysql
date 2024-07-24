e067514d87d557cfac882699018f292d41b32a49
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-07-24 11:14:06 +0300

    added docker

--- apps/back-app/src/main.ts
+++ apps/back-app/src/main.ts
@@ -1,17 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // Укажите здесь URL вашего фронтенда
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
